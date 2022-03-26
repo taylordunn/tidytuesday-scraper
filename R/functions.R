66bd92192f7c2171ce5ec5fdda5d09c6b81b0dfe
taylordunn <taylor@tdunn.ca>
2022-03-26 11:28:10 -0300

    Added `htmltools::HTML()`

--- R/functions.R
+++ R/functions.R
@@ -3,7 +3,7 @@ embed_tweet <- function(id) {
   url <- paste0("https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F", id)
   fromJSON_possibly <- purrr::possibly(~ jsonlite::fromJSON(.)$html,
                                        otherwise = "")
-  tweet <- HTML(fromJSON_possibly(url))
+  tweet <- htmltools::HTML(fromJSON_possibly(url))
 
   class(tweet) <- c("tweet", class(tweet))
   tweet
